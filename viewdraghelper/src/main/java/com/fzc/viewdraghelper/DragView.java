31990731ff0da588ee969bd3e7d1f9bfb980be08
markfan <fanzhengchen@gmail.com>
2016-09-04 02:17:20 +0800

    view drag helper

--- viewdraghelper/src/main/java/com/fzc/viewdraghelper/DragView.java
+++ viewdraghelper/src/main/java/com/fzc/viewdraghelper/DragView.java
@@ -4,27 +4,32 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.view.ViewGroup;
 
 /**
  * Created by fanzhengchen on 9/3/16.
  */
-public class DragView extends FrameLayout {
+public class DragView extends ViewGroup {
 
     private ViewDragHelper mDragHelper;
     private DragCallBack mDragCallBack;
+    private View mContentView;
+    private View mMenuView;
+    private int mCurrentTop = 0;
 
     public DragView(Context context) {
-        super(context, null);
+        this(context, null);
     }
 
     public DragView(Context context, AttributeSet attrs) {
-        super(context, attrs, 0);
+        this(context, attrs, 0);
     }
 
     public DragView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr, 0);
+        this(context, attrs, defStyleAttr, 0);
     }
 
     public DragView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
@@ -33,10 +38,29 @@ public class DragView extends FrameLayout {
         mDragHelper = ViewDragHelper.create(this, 1, mDragCallBack);
     }
 
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mMenuView = getChildAt(0);
+        mContentView = getChildAt(1);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int measureWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int measureHeight = MeasureSpec.getSize(heightMeasureSpec);
+        setMeasuredDimension(measureWidth, measureHeight);
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+    }
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
+        mMenuView.layout(0, 0,
+                mMenuView.getMeasuredWidth(), mMenuView.getMeasuredHeight());
+        mContentView.layout(0, mCurrentTop,
+                mContentView.getMeasuredWidth(),
+                mCurrentTop + mContentView.getMeasuredHeight());
     }
 
     @Override
@@ -44,11 +68,38 @@ public class DragView extends FrameLayout {
         super.onDraw(canvas);
     }
 
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return mDragHelper.shouldInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mDragHelper.processTouchEvent(event);
+        return true;
+    }
+
+    @Override
+    public void computeScroll() {
+        if (mDragHelper.continueSettling(true)) {
+            invalidate();
+        }
+    }
+
     public class DragCallBack extends ViewDragHelper.Callback {
 
         @Override
         public boolean tryCaptureView(View child, int pointerId) {
-            return false;
+            return child == mContentView;
+        }
+
+        @Override
+        public int clampViewPositionVertical(View child, int top, int dy) {
+            Log.d("clamp view position ", top + "");
+//            return super.clampViewPositionVertical(child, top, dy);
+            return Math.max(Math.min(top, mMenuView.getHeight()),0);
         }
     }
+
 }
